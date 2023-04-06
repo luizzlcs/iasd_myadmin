430738c9c263f3d684f30606204e586aa2b951f9
Luiz Carlos <luizzlcs@hotmail.com>
2023-04-06 15:54:00 -0300

    Criado a classe AppThema.

---
+++ lib/themes/app_theme.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppTheme with ChangeNotifier {
+  late ThemeData _myTheme = _darkTheme;
+
+  ThemeData get myTheme {
+    return _myTheme;
+  }
+
+  final _darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.green,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    iconTheme: const IconThemeData(size: 25, color: Colors.red),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(
+          color: Colors.white, fontStyle: FontStyle.italic, fontSize: 15),
+      headlineMedium: TextStyle(
+          color: Color.fromARGB(250, 26, 35, 126),
+          fontWeight: FontWeight.bold,
+          fontSize: 40),
+    ),
+  );
+
+  final _lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.brown,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    iconTheme: const IconThemeData(size: 25, color: Colors.blue),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(
+          color: Colors.black, fontStyle: FontStyle.italic, fontSize: 15),
+      headlineMedium: TextStyle(
+          color: Color.fromARGB(250, 26, 35, 126),
+          fontWeight: FontWeight.bold,
+          fontSize: 40),
+    ),
+  );
+
+  void chandMode() {
+    if (_myTheme == _darkTheme) {
+      _myTheme = _lightTheme;
+    } else {
+      _myTheme = _darkTheme;
+    }
+    notifyListeners();
+  }
+}
