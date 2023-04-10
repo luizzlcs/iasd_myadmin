868e9cb10e5dcad4e4743993647cb032e7243626
Luiz Carlos <luizzlcs@hotmail.com>
2023-04-10 12:56:34 -0300

    Incrementando o método de comparação de senha.

--- lib/app/screens/login/controller/validation_form_login.dart
+++ lib/app/screens/login/controller/validation_form_login.dart
@@ -1,15 +1,23 @@
 // import 'package:iasd_myadmin/app/util/email_validator_util.dart' as EmailValid;
 
+import 'package:flutter/material.dart';
 import 'package:iasd_myadmin/app/util/email_validator_util.dart' as EmailValid;
 
 mixin ValidationFormLogin {
   bool isValidPassword(String? value) => value != null && value.length >= 6;
-  
 
- bool isValidEmail(String? email) {
+  bool isValidPasswordCofirmation(
+      String? value, TextEditingController password) {
+    if (value == password) {
+      return true;
+    }
+    return false;
+  }
+
+  bool isValidEmail(String? email) {
     if (EmailValid.isValid(email)) {
       return true;
     }
     return false;
-  } 
+  }
 }
