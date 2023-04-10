a6f004cd1c164c9f9a3bfa0becf1f3997d1cff11
Luiz Carlos <luizzlcs@hotmail.com>
2023-04-10 15:20:33 -0300

    Ajustado método de comparação de senhas.

--- lib/app/screens/login/controller/validation_form_login.dart
+++ lib/app/screens/login/controller/validation_form_login.dart
@@ -3,12 +3,12 @@
 import 'package:flutter/material.dart';
 import 'package:iasd_myadmin/app/util/email_validator_util.dart' as EmailValid;
 
-mixin ValidationFormLogin {
+mixin ValidationFormLogin{
   bool isValidPassword(String? value) => value != null && value.length >= 6;
 
   bool isValidPasswordCofirmation(
-      String? value, TextEditingController password) {
-    if (value == password) {
+      String? value, String confirmPassword) {
+    if (value == confirmPassword) {
       return true;
     }
     return false;
