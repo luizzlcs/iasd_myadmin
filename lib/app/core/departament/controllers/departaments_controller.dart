fa67fc800518d0c8739854a92cb2ef65a44327f9
Luiz Carlos <luizzlcs@hotmail.com>
2023-04-24 15:57:39 -0300

    Ajustando a controller para salvar os dados.

--- lib/app/core/departament/controllers/departaments_controller.dart
+++ lib/app/core/departament/controllers/departaments_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:iasd_myadmin/app/data/dummy_data.dart';
 import 'package:iasd_myadmin/app/core/departament/model/departaments.dart';
@@ -6,4 +8,34 @@ class DepartamentsController with ChangeNotifier {
   final List<Departaments> _departament = [...dummyData];
 
   List<Departaments> get departament => [..._departament];
+
+  saveDepartaments(Map<String, dynamic> data) {
+    bool hasId = data['id'] != null;
+    final departament = Departaments(
+      id: hasId ? data['id'] : Random().nextDouble().toString(),
+      name: data['name'],
+      description: data['description'],
+      imageUrl: data['image'],
+    );
+    if (hasId) {
+      updateDepartaments(departament);
+    } else {
+      addDepartaments(departament);
+    }
+  }
+
+  void addDepartaments(Departaments departament) {
+    _departament.add(departament);
+    notifyListeners();
+  }
+
+  void updateDepartaments(Departaments departament){
+    int index = _departament.indexWhere((d) => d.id == departament.id);
+
+    if(index >= 0){
+      _departament[index] = departament;
+      notifyListeners();
+    } 
+
+  }
 }
