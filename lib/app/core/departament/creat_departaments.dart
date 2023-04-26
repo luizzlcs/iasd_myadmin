dfbc1c86812d8900ed90a4ea0a523ce0a9e24b98
Luiz Carlos <luizzlcs@hotmail.com>
2023-04-26 17:57:41 -0300

    adicionado nova estratégia para salvar os dados e removido estratégia anterior.

--- lib/app/core/departament/creat_departaments.dart
+++ lib/app/core/departament/creat_departaments.dart
@@ -1,21 +1,26 @@
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:iasd_myadmin/app/core/departament/controllers/departaments_controller.dart';
-import 'package:iasd_myadmin/app/core/departament/model/activity.dart';
+import 'package:iasd_myadmin/app/core/departament/model/departaments.dart';
 import 'package:iasd_myadmin/app/util/app_routes.dart';
 import 'package:provider/provider.dart';
 
 class CreatDepartaments extends StatefulWidget {
-  const CreatDepartaments({Key? key}) : super(key: key);
+  const CreatDepartaments({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<CreatDepartaments> createState() => _CreatDepartamentsState();
 }
 
 class _CreatDepartamentsState extends State<CreatDepartaments> {
+  TextEditingController nameEC = TextEditingController();
+  TextEditingController descricaoEC = TextEditingController();
+  TextEditingController imageEC = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
-  final _formData = <String, dynamic>{};
-  final List<Activity> _formActivity = [];
+
   _submitForm() {
     debugPrint('Salvando dados');
     final isValid = _formKey.currentState?.validate() ?? false;
@@ -23,19 +28,13 @@ class _CreatDepartamentsState extends State<CreatDepartaments> {
     if (!isValid) {
       return;
     }
-
-    _formKey.currentState?.save();
-
-    Provider.of<DepartamentsController>(context, listen: false)
-        .saveDepartaments(_formData, _formActivity );
-    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cadastro de departamento'),
+        title: const Text('Cadastro de departamentos'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -47,66 +46,89 @@ class _CreatDepartamentsState extends State<CreatDepartaments> {
                 height: 20,
               ),
               TextFormField(
+                controller: nameEC,
                 decoration: const InputDecoration(
                     isDense: true,
                     labelText: 'Nome',
                     hintText: 'Nome do departamento',
                     border: OutlineInputBorder()),
                 textInputAction: TextInputAction.next,
-                onSaved: (name) => _formData['name'] = name ?? '',
-                validator: (_value) {
-                  final value = _value ?? '';
-                  if (value.trim().isEmpty) {
+                validator: (value) {
+                  final values = value ?? '';
+                  if (values.trim().isEmpty) {
                     return 'É necessário informar o nome do departamento';
                   }
+                  return null;
                 },
               ),
               const SizedBox(
                 height: 15,
               ),
               TextFormField(
+                controller: descricaoEC,
                 decoration: const InputDecoration(
                     isDense: true,
                     labelText: 'Decrição',
                     hintText: 'Descrição do departamento',
                     border: OutlineInputBorder()),
                 textInputAction: TextInputAction.next,
-                onSaved: (descricao) =>
-                    _formData['description'] = descricao ?? '',
-                validator: (_value) {
-                  final value = _value ?? '';
-                  if (value.trim().isEmpty) {
+                validator: (value) {
+                  final values = value ?? '';
+                  if (values.trim().isEmpty) {
                     return 'É necessário fazer uma descrição para o departamento';
                   }
+                  return null;
                 },
               ),
               const SizedBox(
                 height: 15,
               ),
               TextFormField(
-                  decoration: const InputDecoration(
-                      isDense: true,
-                      labelText: 'Imgaem',
-                      hintText: 'Imagem do departamento',
-                      border: OutlineInputBorder()),
-                  textInputAction: TextInputAction.done,
-                  onFieldSubmitted: (_) => _submitForm(),
-                  onSaved: (image) => _formData['image'] = image ?? ''),
+                controller: imageEC,
+                decoration: const InputDecoration(
+                    isDense: true,
+                    labelText: 'Imgem',
+                    hintText: 'Imagem do departamento',
+                    border: OutlineInputBorder()),
+                textInputAction: TextInputAction.done,
+                onFieldSubmitted: (_) => _submitForm(),
+              ),
               const SizedBox(
                 height: 15,
               ),
               ElevatedButton.icon(
                 style:
                     ElevatedButton.styleFrom(minimumSize: const Size(300, 50)),
-                onPressed: () => _submitForm(),
+                onPressed: () {
+                  final name = nameEC.text;
+                  final descricao = descricaoEC.text;
+                  final image = imageEC.text;
+
+                  Departaments newDepartaments = Departaments(
+                    id: Random().nextDouble().toString(),
+                    name: name,
+                    description: descricao,
+                    imageUrl: image,
+                    activity: [],
+                  );
+                  debugPrint(newDepartaments.id);
+                  debugPrint(newDepartaments.name);
+                  debugPrint(newDepartaments.imageUrl);
+                  Provider.of<DepartamentsController>(context, listen: false)
+                      .addDepartaments(newDepartaments);
+                  Navigator.pop(context);
+                },
                 icon: const Icon(Icons.save),
                 label: const Text('Salvar'),
               ),
               const SizedBox(
-                 height: 15,
+                height: 15,
               ),
-              TextButton(onPressed: ()=> Navigator.of(context).pushNamed(AppRoutes.activityScreen)
-              , child: const Text('Criar atividades'),)
+              TextButton(
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(AppRoutes.activityScreen),
+                child: const Text('Criar atividades'),
+              )
             ],
           ),
         ),
