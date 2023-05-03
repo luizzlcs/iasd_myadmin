f1b24d75cfced0fbd444e023ba3b6495f2ed5408
Luiz Carlos <luizzlcs@hotmail.com>
2023-05-02 23:23:29 -0300

    Ajustando GridView

--- lib/app/components/grid_departaments.dart
+++ lib/app/components/grid_departaments.dart
@@ -17,122 +17,66 @@ class GridDepartaments extends StatelessWidget {
 
     final isDesktop = Responsive.isDesktop(context);
 
-    return GridView.builder(
-      padding: const EdgeInsets.all(defaultPadding),
-      itemCount: departament.departament.length,
-      itemBuilder: (context, index) {
-        Departaments depart = departament.departament[index];
-        return Ink(
-          decoration: BoxDecoration(
-            color: Provider.of<AppTheme>(context).isDark()
-                ? Colors.white.withOpacity(0.2)
-                : Colors.black.withOpacity(0.5),
-            borderRadius:  BorderRadius.only(
-              topRight: Radius.circular(dimension.height * .3),
-              topLeft: Radius.circular(dimension.height * .3),
-              bottomLeft: Radius.circular(dimension.height * .01),
-              bottomRight: Radius.circular(dimension.height * .3),
-            ),
-          ),
-          child: InkWell(
-            focusColor: Colors.purple,
-            splashColor: Colors.pink.withOpacity(0.5),
-            highlightColor: Colors.blue,
-            borderRadius: BorderRadius.circular(200),
-            onTap: () {
-              Navigator.of(context)
-                  .pushNamed(AppRoutes.secretaria, arguments: depart);
-            },
-            child: Stack(children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(
-                    child: SizedBox(
-                      child: CircleAvatar(
-                        radius: isDesktop ? dimension.height *.17 : 52,
-                        backgroundImage: ResizeImage(
-                          NetworkImage(depart.imageUrl),
-                          width: (dimension.width * 0.60).toInt() ,
-                          height: (dimension.width * 0.60).toInt() ,
-                        ),
-                      ),
+    return LayoutBuilder(builder: (context, constraints) {
+      double scaleFactor = constraints.maxWidth / 100.0;
+      return GridView.builder(
+        padding: const EdgeInsets.all(defaultPadding),
+        itemCount: departament.departament.length,
+        itemBuilder: (context, index) {
+          Departaments depart = departament.departament[index];
+          return Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Positioned(
+                top: 0,
+                left: 0,
+                child: Container(
+                  width: scaleFactor * 18,
+                  height: scaleFactor * 10,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.1),
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(depart.imageUrl)),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(32),
                     ),
                   ),
-                ],
+                  padding: const EdgeInsets.only(left: 32, top: 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [],
+                  ),
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Center(
-                    child: Padding(
-                      padding: EdgeInsets.only(top: isDesktop ? 220 : 82),
-                      child: Container(
-                        height: isDesktop ?  40 : 35,
-                        width: isDesktop ? 180 : 80,
-                        decoration: const BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius: BorderRadius.only(
-                            bottomRight: Radius.circular(50),
-                            topLeft: Radius.circular(5),
-                            bottomLeft: Radius.circular(5),
-                            topRight: Radius.circular(5),
-                          ),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 10),
-                          child: Text(
-                            overflow: TextOverflow.ellipsis,
-                            depart.name,
-                            style: Provider.of<AppTheme>(context, listen: false)
-                                    .isDark()
-                                ? const TextStyle(color: Colors.white)
-                                : const TextStyle(color: Colors.white),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
+              Positioned(
+                top: scaleFactor * 8,
+                left: scaleFactor * 1,
+                child: Container(
+                  width: scaleFactor * 16,
+                  height: scaleFactor * 5,
+                  decoration: const BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7),
                     ),
                   ),
-                  Center(
-                    child: Padding(
-                      padding: EdgeInsets.only(top: isDesktop ? 220 : 82),
-                      child: Container(
-                        height: isDesktop ? 40 : 40,
-                        width: isDesktop ? 50 : 30,
-                        decoration: const BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(50),
-                            bottomLeft: Radius.circular(5),
-                            topRight: Radius.circular(5),
-                            bottomRight: Radius.circular(5),
-                          ),
-                        ),
-                        child: IconButton(
-                          alignment: Alignment.topCenter,
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.edit_note_sharp,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [],
                   ),
-                ],
+                ),
               ),
-            ]),
-          ),
-        );
-      },
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: isDesktop ? 5 : 3,
-        childAspectRatio: 4 / 4,
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 30,
-      ),
-    );
+            ],
+          );
+        },
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: isDesktop ? 5 : 3,
+          childAspectRatio: 4 / 4,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 30,
+        ),
+      );
+    });
   }
 }
